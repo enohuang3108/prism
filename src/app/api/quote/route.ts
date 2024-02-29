773a57ea4f7988aaddb681f06f00707bd5bc5d82
Huai <huai@Huais-Mac-mini.local>
2024-02-29 23:28:32 +0800

    stock form

--- src/app/api/quote/route.ts
+++ src/app/api/quote/route.ts
@@ -1,16 +1,41 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { type NextRequest } from "next/server";
 import yahooFinance from "yahoo-finance2";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get("query") || "AAPL";
+  const stock = searchParams.get("stock");
 
-  const quote = await yahooFinance.quote(query);
+  if (stock == null) {
+    return Response.json(
+      {
+        error: "Missing parameter",
+      },
+      {
+        status: 400,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
+    );
+  }
+
+  const quote = await yahooFinance.quote(stock);
   const { regularMarketPrice, currency } = quote;
 
-  const json = {
-    regularMarketPrice,
-  };
-
-  return NextResponse.json(json);
+  return Response.json(
+    {
+      regularMarketPrice,
+      currency,
+    },
+    {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    }
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,54 +5,82 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-export default function Page() {
+const Asset = () => {
+  const [asset, setAsset] = useState(0);
   const formSchema = z.object({
     stock: z.string().min(1).max(10),
+    shares: z.coerce.number().min(0).max(9999),
   });
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       stock: "AAPL",
+      shares: 1,
     },
   });
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
-    await fetch(`/api/quote?query=${values.stock}`, {
+    await fetch(`/api/quote?stock=${values.stock}`, {
       method: "GET",
-    }).then((res) => {
-      console.log(res);
-    });
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setAsset(json.regularMarketPrice * values.shares);
+      });
   }
 
   return (
-    <main>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="stock"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Stock</FormLabel>
-                <FormControl>
-                  <Input placeholder="ex:AAPL" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-row space-x-1"
+      >
+        <FormField
+          control={form.control}
+          name="stock"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="ex:AAPL" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="shares"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="ex:10.001" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+        {asset}
+      </form>
+    </Form>
+  );
+};
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center space-y-1">
+      <Asset />
+      <Asset />
     </main>
   );
 }
